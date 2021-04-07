f6c7c58c8baf88ea45ab838aea39e24dd216f8e6
keyestudio <53899501+keyestudio2019@users.noreply.github.com>
2021-04-07 13:33:40 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -372,6 +372,66 @@ namespace k_Bit {
     export function PH(): number {
         return pins.analogReadPin(AnalogPin.P1);  
     }
+    let irstate:number;
+    let IR_state:number;
+
+    //% advanced=true shim=maqueenIRV2::irCode
+    function irCode(): number {
+        return 0;
+    }
+
+    //% weight=5
+    //% group="micro:bit(v2)"
+    //% blockId=IR_readv2 block="read IR key value"
+    export function IR_readV2(): number {
+        return valuotokeyConversion();
+    }
+    //% weight=2
+    //% group="micro:bit(v2)"
+    //% blockId=IR_callbackUserv2 block="on IR received"
+    //% draggableParameters
+    export function IR_callbackUserV2(cb: (message: number) => void) {
+        IR_state = 1;
+        control.onEvent(11, 22, function() {
+            cb(irstate)
+        }) 
+    }
+    function valuotokeyConversion():number{
+        let irdata:number;
+        switch(irCode()){
+            case 0xb946:irdata = 12;break;
+            case 0xbb44:irdata = 13;break;
+            case 0xea15:irdata = 14;break;
+            case 0xbc43:irdata = 15;break;
+            case 0xbf40:irdata = 16;break;
+            case 0xad52:irdata = 0;break;
+            case 0xe916:irdata = 1;break;
+            case 0xe619:irdata = 2;break;
+            case 0xf20d:irdata = 3;break;
+            case 0xf30C:irdata = 4;break;
+            case 0xe718:irdata = 5;break;
+            case 0xa15e:irdata = 6;break;
+            case 0xf708:irdata = 7;break;
+            case 0xe31c:irdata = 8;break;
+            case 0xa55a:irdata = 9;break;
+            case 0xbd42:irdata = 10;break;
+            case 0xb54a:irdata = 11;break;
+            default:
+            irdata = -1;
+        }
+        return irdata;
+        //return irCode();
+    }
+    basic.forever(() => {
+        if(IR_state == 1){
+            irstate = valuotokeyConversion();
+            if(irstate != -1){
+                control.raiseEvent(11, 22)
+            }
+        }
+        
+        basic.pause(20);
+    })
 
     
 }
